307988a374d6f09c5017a7fc4713407fd65d10b5
maxhha <devmaxhha@gmail.com>
2021-04-28 18:07:37 +0300

    wip

--- lab_02/inc/container.h
+++ lab_02/inc/container.h
@@ -7,22 +7,26 @@ public:
     Container(size_t size = 0) : size(size){};
 
     virtual size_t
-    getSize() const noexcept
-    {
-        return size;
-    };
+    getSize() const noexcept;
 
     virtual bool
-    isEmpty() const noexcept
-    {
-        return size == 0;
-    }
+    isEmpty() const noexcept;
 
     virtual ~Container() = 0;
 
 protected:
     size_t size = 0;
 };
+
+size_t Container::getSize() const noexcept
+{
+    return size;
+}
+
+bool Container::isEmpty() const noexcept
+{
+    return size == 0;
+}
 
 Container::~Container() {}
 
