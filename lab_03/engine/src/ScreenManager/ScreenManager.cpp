44a32d98236385142f839ccaaefe52ab42aadb18
maxhha <devmaxhha@gmail.com>
2021-05-18 11:34:43 +0300

    screen manager with vector

--- lab_03/engine/src/ScreenManager/ScreenManager.cpp
+++ lab_03/engine/src/ScreenManager/ScreenManager.cpp
@@ -1,22 +1,21 @@
 #include "ScreenManager.h"
 
-ScreenManager::ScreenManager(std::shared_ptr<ScreenCreator> &_creator)
+ScreenManager::ScreenManager(std::shared_ptr<ScreenCreator> &_creator) : creator(_creator), screens(new Vector<std::shared_ptr<Screen>>())
 {
-    creator = _creator;
-    screens_size = 0;
 }
 
 std::shared_ptr<Screen> ScreenManager::addScreen(int x, int y, int width, int height)
 {
     // #TODO: throw exception on no mem
-    screen = creator->createScreen(x, y, width, height);
-    screens_size++;
+    auto screen = creator->createScreen(x, y, width, height);
+    screens->pushBack(screen);
 
     return screen;
 }
+
 std::shared_ptr<Screen> ScreenManager::getScreen(int i)
 {
-    return screen;
+    return (*screens)[i];
 }
 
 ScreenIterator ScreenManager::getIterator()
@@ -27,7 +26,10 @@ ScreenIterator ScreenManager::getIterator()
 
 void ScreenManager::render()
 {
-    screen->render();
+    for (auto &screen : *screens)
+    {
+        screen->render();
+    }
 
     return;
 }
